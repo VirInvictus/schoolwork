35c70c87713e6b47816bcffe64f35325dd9837ad
Brandon LaRocque <larocque.brandon@gmail.com>
2025-11-09 09:52:33 -0500

    Delete Module 1 directory

--- Module 1/ThreeMessages.java
+++
@@ -1,9 +0,0 @@
-package module2;
-
-public class ThreeMessages {
-	public static void main(String[] args) {
-		System.out.println("Programming is fun!");
-		System.out.println("Fundemanetals First");
-		System.out.println("Problem Driven");
-	}
-}
--- Module 1/Welcome.java
+++
@@ -1,7 +0,0 @@
-package module2;
-
-public class Welcome {
-	public static void main(String[] args) {
-		System.out.println("hello world");
-	}
-}
